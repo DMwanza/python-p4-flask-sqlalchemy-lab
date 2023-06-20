9d0136181f16a55c6c7c4da07e4c1f826f1f024e
Dennis Musembi <dennismusembi344@gmail.com>
2023-06-21 00:39:24 +0300

    lab done

--- server/app.py
+++ server/app.py
@@ -19,11 +19,24 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id==id).first()
 
+    if not animal:
+        response_body='<h1>404 owner not found</h1>'
+        response=make_response(response_body,404)
+        return response
+    response_body=f'''
+    <li>animal ID {{ animal.id }}, name {{ animal.name }}, species {{ animal.species }}, zookeeper {{ animal.zookeeper.name }}, enclosure {{ animal.enclosure.environment }}</li>
+    '''
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id==id).first()
+
+    if not zookeeper:
+        response_body='<h1>404 pet not found</h1>'
+        response=make_response(response_body,404)
+        return response
+    response_body=f'''
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
--- server/models.py
+++ server/models.py
@@ -9,15 +9,31 @@ db = SQLAlchemy(metadata=metadata)
 
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
-
     id = db.Column(db.Integer, primary_key=True)
-
+    name = db.Column(db.String, unique=True)
+    birthday=db.Column(db.Date, nullable=False)
+    animals=db.relationship('Animal',backref='zookeepers',lazy=True)
+    
+    def __repr__(self):
+        return f"<Zookeeper ID: {self.id}, Name: {self.name}, Birthday: {self.birthday}>"
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String, nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    animals=db.relationship('Animal',backref='enclosures',lazy=True)
 
+    def __repr__(self):
+        return f"<Enclosure ID: {self.id}, Environment: {self.environment}, Open to Visitors: {self.open_to_visitors}>"
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String,unique=True)
+    species = db.Column(db.String(100))
+    zookeeper_id=db.Column(db.Integer,db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    def __repr__(self):
+        return f"<Animal ID: {self.id}, Name: {self.name}, Species: {self.species}>"
